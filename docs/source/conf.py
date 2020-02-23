97a4abba3d3a994065476407ae03fc32aa1e2883
slbabu <sukruthlb007@gmail.com>
2020-02-23 14:44:50 -0800

    5th

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,13 @@
 
 # -- Project information -----------------------------------------------------
 
+from sphinx.highlighting import lexers
+from pygments.lexers.web import PhpLexer
+lexers['php'] = PhpLexer(startinline=True, linenos=1)
+lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
+primary_domain = 'php'
+
+
 project = 'lorawan'
 copyright = '2020, Sukruth Babu'
 author = 'Sukruth Babu'
